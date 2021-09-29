1fbd43f9f15856957908e8110526a6cd89de6bf6
Bowo1 <aliftriwibowo1@gmail.com>
2021-09-29 23:02:01 +0700

    test

---
+++ src/cekvip.js
@@ -0,0 +1,13 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nama bot* : YUKINIKO.BOT
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ACTIVE*
+────────────────── 
+*Status Bot:* *Online*
+────────────────── `
+}
+exports.cekvip = cekvip
---
+++ src/daftarvip.js
@@ -0,0 +1,17 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*HARGA DAFTAR VIP :*
+-Rp. 10K > Akses Fitur ViP
+-Rp. 15K > Fitur VIP + Masukin Bot KeGrup Kalian!
+
+*JIKA INGIN DAFTAR VIP :*
+*Chat Owner BOT :*
+_wa.me/6287720646259 atau ketik *${prefix}owner*
+
+*NOTE*
+
+*GRUP WHATSAPP BOT :*
+_https://chat.whatsapp.com/JRlnBMbOvxq9Y0xzBJGgfI `
+}
+exports.daftarvip = daftarvip
